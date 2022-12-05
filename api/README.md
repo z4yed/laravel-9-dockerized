692891cacdb057b95cd62f9c036bcf0056ac73a0
Zayed <zayed.cs3@gmail.com>
2022-12-05 20:06:56 +0600

    Initial commit

---
+++ api/README.md
@@ -0,0 +1,7 @@
+## What is this?
+
+* This is an empty laravel(9) project
+
+## Why it is empty?
+* It is possible that you don't have the environment setup locally to create a new laravel project.
+* If you want to start a new laravel project and want to **dockerize** it from the very beginning, you can easily close this empty project.
---
+++ api/resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                margin: 0;
+                min-height: 100vh;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>APP is running</h1>
+    </body>
+</html>
